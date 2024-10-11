0447b656a9dbddd2b96107638250b4c87e24cb66
P1ecful <p1ecfuldog@gmail.com>
2024-10-11 23:04:50 +0300

    Changes to be committed: 	modified:   __pycache__/main.cpython-312.pyc 	modified:   main.py 	new file:   src/domain/register.py 	new file:   src/storage /storage.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+
+def main():
+    app = FastAPI()
+    app.mount("/", StaticFiles(directory="src/static/", html=True))
+
+if __name__ == "__main__":
+    main()
  
-app = FastAPI()
-app.mount("/", StaticFiles(directory="src/static/", html=True))
  
